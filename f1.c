967677af3b560937228e113fd6111e8b21638da4
gayathrivshenoy <gayathrivshenoybca25@rvu.edu.in>
2026-05-13 12:27:01 +0530

    First commit

---
+++ f1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, sum;
+
+    printf("Enter two integers: ");
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    sum = a + b;
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}  
